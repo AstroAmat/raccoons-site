7163bb6c2e926bd975d4ca9f6d5aa8d47dc4a396
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2019-09-23 13:10:36 -0500

    Added privacy advice and terms and conditions

--- raccoons/core/urls.py
+++ raccoons/core/urls.py
@@ -10,4 +10,14 @@ urlpatterns = [
         view=TemplateView.as_view(template_name='core/index.html'),
         name='index'
     ),
+    path(
+        route='terminos-y-condiciones',
+        view=TemplateView.as_view(template_name='core/terms.html'),
+        name='terms'
+    ),
+    path(
+        route='aviso-privacidad',
+        view=TemplateView.as_view(template_name='core/privacy.html'),
+        name='privacy'
+    ),
 ]
